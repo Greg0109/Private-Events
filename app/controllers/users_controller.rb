de62ebc623a5f2e033ea10ceb6f3846495fd1980
Greg0109 <greg.rabago@gmail.com>
2021-03-16 10:58:56 +0100

    Users, events, attendees linked. Can make users

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,40 @@
+class UsersController < ApplicationController
+    def index
+        @users = User.all
+    end
+
+    
+    def new
+        @user = User.new
+    end
+    
+    def create
+        @user = User.new(user_params)
+        if @user.save
+            session[:userid] = @user.id
+            session[:username] = @user.username
+            redirect_to root_path
+        else
+            render 'new'
+        end
+    end
+    
+    def show
+        if logged_in?
+            @user = User.find(params[:id])
+            @events = @user.events
+        else
+            redirect_to users_new_path
+        end
+    end
+
+    def logout
+        reset_session
+        redirect_to root_path
+    end
+
+    private
+    def user_params
+        params.require(:user).permit(:username)
+    end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,11 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  get '/events' => 'events#index'
+  get '/events/:id' => 'events#show', as: :event
+  get '/users' => 'users#index'
+  get '/users/show' => 'users#show'
+  get '/users/new' => 'users#new'
+  post 'users' => 'users#create'
+  delete '/users/logout' => 'users#logout'
+  root "events#index"
 end
