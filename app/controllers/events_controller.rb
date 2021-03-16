bbc6f69674e82eceb1b159fc13b0c3b5cfb37c34
Greg0109 <greg.rabago@gmail.com>
2021-03-16 11:37:43 +0100

    Create event initial

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -5,6 +5,24 @@ class EventsController < ApplicationController
 
     def show
         @event = Event.find(params[:id])
-        @attendees = @event.attendees
+        # @attendees = @event.attendees
+    end
+    
+    def new
+        @event = Event.new
+    end
+
+    def create
+        @event = Event.new(event_params)
+        if @event.save
+            redirect_to root_path
+        else
+            render 'new'
+        end
+    end
+
+    private
+    def event_params
+        params.require(:event).permit(:title,:description,:date)
     end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   get '/events' => 'events#index'
-  get '/events/:id' => 'events#show', as: :event
+  get '/events/show/:id' => 'events#show', as: :event
+  get '/events/new' => 'events#new'
+  post 'events' => 'events#create'
   get '/users' => 'users#index'
   get '/users/show/:id' => 'users#show', as: :user
   get '/users/new' => 'users#new'
