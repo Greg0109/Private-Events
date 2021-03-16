d3773130a2196ae2c0df4cfc3505ad66966dfb1a
Greg0109 <greg.rabago@gmail.com>
2021-03-16 14:15:50 +0100

    Add validations

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,4 +1,7 @@
 class Event < ApplicationRecord
     has_many :atendees
     has_many :users, through: :atendees
+
+    validates :title, presence: true, length: { minimum: 5 }
+    validates :description, presence: true, length: { minimum: 10 }
 end
