97b32baee47e763f023d804aaa9330289ceac4dd
Greg0109 <greg.rabago@gmail.com>
2021-03-17 18:48:59 +0100

    Add requested changes

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,6 @@
 class User < ApplicationRecord
   has_many :atendees, foreign_key: :atendee_id
-  has_many :events, through: :atendees, foreign_key: :creator_id
+  has_many :events, through: :atendees, foreign_key: :user_id
 
   validates :username, presence: true, uniqueness: true
 end
