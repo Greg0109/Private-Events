1a210bf37d27ca90392b50fcb39d1f4b8072e9e5
Greg0109 <greg.rabago@gmail.com>
2021-03-17 18:48:01 +0100

    Add requested changes

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,6 +1,7 @@
 class Event < ApplicationRecord
   has_many :atendees, foreign_key: :event_id
   has_many :users, through: :atendees
+  belongs_to :user, class_name: 'User'
 
   validates :title, presence: true, length: { minimum: 5 }
   validates :description, presence: true, length: { minimum: 10 }
